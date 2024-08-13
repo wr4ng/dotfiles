a287620dfa4e5c7bb5fa2667907da5cf392d0075
Mads Christian Wrang Nielsen <madc@ip-192-168-1-27.eu-west-1.compute.internal>
2024-08-13 19:36:03 +0200

    make catppuccin comment color more readable

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -4,6 +4,15 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+		-- Change comment color
+		require("catppuccin").setup({
+			custom_highlights = function(colors)
+				return {
+					Comment = { fg = colors.flamingo },
+				}
+			end,
+		})
+
 		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
